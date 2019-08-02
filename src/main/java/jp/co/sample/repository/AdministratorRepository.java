ae1285c86d233b9ea43db98d321d3c3fbf55e362
yamadeayaka <ayaka.yamade@rakus-partners.co.jp>
2019-08-02 14:18:37 +0900

    AddministratorRepositoryを作成しました

--- src/main/java/jp/co/sample/repository/AdministratorRepository.java
+++ src/main/java/jp/co/sample/repository/AdministratorRepository.java
@@ -1,8 +1,11 @@
 package jp.co.sample.repository;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
@@ -26,7 +29,21 @@ public class AdministratorRepository {
 	
 	public void save(Administrator administrator) {
 		SqlParameterSource param = new BeanPropertySqlParameterSource(administrator);
+	    String sql = "INSERT INTO administrator(name,maill_address,password) VALUES(:name, :mailAddress, :password)";
+	    template.update(sql, param);
+	    		
+	}
 	
+	public List<Administrator> findByMailAddressAndPassword(String mailAddress, String password) {
+	    	 String sql = "SELECT id,name,mail_address,password FROM administrators WHERE mail_address = :mailAddress, password = :password";
+	    	 SqlParameterSource param = new MapSqlParameterSource().addValue("mail_address",mailAddress).addValue("password", password);
+	    	 List<Administrator> administrator = template.query(sql,param,ADMINISTRATOR_ROW_MAPPER);
+	    	 if(administrator == null) {
+	    		 return null;
+	    	 }else {
+	    		 return administrator;
+	    	 }
+	      
 	}
 
 }
