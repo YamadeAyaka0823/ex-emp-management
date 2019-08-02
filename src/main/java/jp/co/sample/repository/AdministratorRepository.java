91d314625ef166011e4c3ca35c75b1ceea55817b
yamadeayaka <ayaka.yamade@rakus-partners.co.jp>
2019-08-02 12:40:42 +0900

    AdministratorとEmployeeドメインを作成しました.

---
+++ src/main/java/jp/co/sample/repository/AdministratorRepository.java
@@ -0,0 +1,32 @@
+package jp.co.sample.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
+
+import jp.co.sample.domain.Administrator;
+
+@Repository
+public class AdministratorRepository {
+	
+	@Autowired
+	private NamedParameterJdbcTemplate template;
+	
+	private static final RowMapper<Administrator> ADMINISTRATOR_ROW_MAPPER = (rs,i)->{
+		Administrator administrator = new Administrator();
+		administrator.setId(rs.getInt("id"));
+		administrator.setName(rs.getString("name"));
+		administrator.setMailAddress(rs.getString("mail_address"));
+		administrator.setPassword(rs.getString("password"));
+		return administrator;
+	};
+	
+	public void save(Administrator administrator) {
+		SqlParameterSource param = new BeanPropertySqlParameterSource(administrator);
+	
+	}
+
+}
